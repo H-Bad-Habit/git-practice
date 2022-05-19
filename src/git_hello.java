7cac09ae1be8645718054e0690a98ed9ef7af42c
Bad-Habit <972156795@qq.com>
2022-05-19 21:31:35 +0800

    idea git second commit

--- src/git_hello.java
+++ src/git_hello.java
@@ -5,5 +5,9 @@
 public class git_hello {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello gitee!!!");
+        System.out.println("hello github!!!");
+        System.out.println("hello git123!!!");
+        System.out.println("hello git666!!!");
     }
 }
